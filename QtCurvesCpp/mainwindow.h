0bb7bdbdb563be4fbc08f724eb8497f3aa52f1c1
juangpc <juangpc@gmail.com>
2019-07-20 21:03:53 -0500

    update comments on Qt structure

--- QtCurvesCpp/mainwindow.h
+++ QtCurvesCpp/mainwindow.h
@@ -3,20 +3,22 @@
 
 #include <QMainWindow>
 
+//these two MainWindow classes are different because they are in
+//a different namespace
 namespace Ui {
 class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT //this object is mandatory and lets compiler that the class will benefit of all Qt framework functionality
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr); //constructor
+    ~MainWindow(); //destructor
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui; //pointer to MainWindow object used to access GUI elements.
 };
 
 #endif // MAINWINDOW_H
